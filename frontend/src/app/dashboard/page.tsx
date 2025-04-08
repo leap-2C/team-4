2a6fa2c4e588f99989e000ec541dda1c8184fb46
InderN10 <midtulah2000@gmail.com>
2025-04-08 11:20:59 +0800

    commit

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DashboardSideBar from './_components/DashboardSideBar'
+
+
+function page() {
+  return (
+    <div>
+        <DashboardSideBar />
+        dashboard
+    </div>
+  )
+}
+export default page

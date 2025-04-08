74b088d923682641d250e577b6a4506ffa646608
24LP2336 24LP2336 <24LP2336@G304-CC0218.local>
2025-04-08 09:56:44 +0800

    commit

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-
-import { Coffee } from 'lucide-react';
-
+import Signup from './_components/sign_up';
 const Page = () => {
     return (
         <div>
-            <div className='w-1/2 h-[100vh] bg-[#FBBF24]'>
-                <div className='flex flex-row gap-2 items-center'>
-                    <Coffee />
-                    <p className='text-black text-bold text-[20px]'>Buy me coffee</p>
-                </div>
-            </div>
-            <div>
-
-            </div>
+            <Signup />
         </div>
     );
 };

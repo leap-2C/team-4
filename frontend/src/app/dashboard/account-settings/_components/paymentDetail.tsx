dfc1a711563062ace0621e009d9b073baaafea70
24LP2336 24LP2336 <24LP2336@G304-CC0218.local>
2025-04-09 10:43:43 +0800

    commit

---
+++ frontend/src/app/dashboard/account-settings/_components/paymentDetail.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useRef } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+function setPassword() {
+  return (
+    <div className="flex flex-col gap-4 border border-[#E4E4E7] rounded-[8px] p-4">
+      <p className="font-bold">Payment details</p>
+      <div>
+        <p className="text-[14px] font-medium">Select country</p>
+        
+      </div>
+    
+      <Button className="w-[602px] mt-4" variant="default">
+        Save changes
+      </Button>
+    </div>
+  );
+}
+export default setPassword;

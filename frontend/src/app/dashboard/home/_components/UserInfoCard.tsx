5d239ce393a6a18b6e51664310cbce3d1177ce79
InderN10 <midtulah2000@gmail.com>
2025-04-09 12:19:44 +0800

    tsarain bolsoo odoo function

--- frontend/src/app/dashboard/home/_components/UserInfoCard.tsx
+++ frontend/src/app/dashboard/home/_components/UserInfoCard.tsx
@@ -1,13 +1,44 @@
+"use client"
+
 import React from "react";
 import { Copy } from "lucide-react";
-import { ChevronDown } from "lucide-react";
+import { useState } from "react";
+import { Check, ChevronDown, ChevronsUpDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
 
+const frameworks = [
+  {
+    value: "Last 30 days",
+    label: "Last 30 days",
+  },
+  {
+    value: "Last 90 days",
+    label: "Last 90 days",
+  },
+  {
+    value: "All time",
+    label: "All time",
+  }
+];
+
 function UserInfoCard() {
+
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
   return (
     <div className=" w-[907px] h-[257px] border-[1px] rounded-[8px] border-gray-300 flex flex-col gap-3 p-6 ">
       <div className="w-[859px] h-[48px] flex justify-between ">
@@ -32,10 +63,45 @@ function UserInfoCard() {
         <div className="flex items-center gap-4">
           <p className="text-[20px] font-semibold">Earnings</p>
           <div>
-            <Popover>
-              <PopoverTrigger>Open</PopoverTrigger>
-              <PopoverContent>
-                Place content for the popover here.
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className="w-[200px] justify-between"
+                >
+                  {value
+                    ? frameworks.find((framework) => framework.value === value)
+                        ?.label
+                    : "Last 30 days"}
+                  <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-[200px] p-0">
+                <Command>
+                  <CommandInput placeholder="Search framework..." />
+                  <CommandList>
+                    <CommandEmpty>No framework found.</CommandEmpty>
+                    <CommandGroup>
+                      {frameworks.map((framework) => (
+                        <CommandItem
+                          key={framework.value}
+                          value={framework.value}
+                          onSelect={(currentValue) => {
+                            setValue(
+                              currentValue === value ? "" : currentValue
+                            );
+                            setOpen(false);
+                          }}
+                        >
+                    
+                          {framework.label}
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
               </PopoverContent>
             </Popover>
           </div>

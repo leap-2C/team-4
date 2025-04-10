3b117d13d28eccf239f314104492bdb695dc19a6
Uilsee <uilseeu437@gmail.com>
2025-04-10 12:13:39 +0800

    updated Authorization

--- Backend/src/MiddleWare/Authorization.ts
+++ Backend/src/MiddleWare/Authorization.ts
@@ -2,20 +2,11 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { Request, Response, NextFunction } from "express";
 
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: { id: string; email: string };
-    }
-  }
-}
-
 dotenv.config();
 
 const SECRET_KEY = process.env.JWT_SECRET_KEY as string;
 
-export const Authorization = (req:Request, res:Response, next:NextFunction) => {
+export const Authorization = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization;
 
   if (!token) {
@@ -26,7 +17,7 @@ export const Authorization = (req:Request, res:Response, next:NextFunction) => {
 
   try {
     const user = jwt.verify(tokenValue, SECRET_KEY) as { id: string; email: string };
-    req.user = user;
+    (req as any).user = user; 
     next();
   } catch (error) {
     return res.status(401).json({ message: "Unauthorized" });
--- Backend/src/routers/User-router.ts
+++ Backend/src/routers/User-router.ts
@@ -2,9 +2,10 @@ import express from "express";
 import createUser from "../controller/User/CreateUser";
 import login from "../controller/User/Login";
 import { deleteUser  } from "../controller/User/DeleteUser";
+import { Authorization } from "../MiddleWare/Authorization";
 
 export const userRouter = express.Router();
 
 userRouter.post("/", createUser);
 userRouter.get("/login", login);
-userRouter.delete("/:id", deleteUser);
+userRouter.delete("/:id", deleteUser,);

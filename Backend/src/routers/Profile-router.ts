5c5c252787b78762f39287e82329767fc91ae740
Uilsee <uilseeu437@gmail.com>
2025-04-11 11:49:06 +0800

    router fixing

---
+++ Backend/src/routers/Profile-router.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { updateProfile } from '../controller/Profile/UpdateProfile';
+import { getProfile } from '../controller/Profile/GetProfile';
+import { createProfile } from '../controller/Profile/CreateProfile';
+import { Authorization } from '../MiddleWare/Authorization';
+
+export const profileRouter = express.Router();
+
+profileRouter.post('/', Authorization, createProfile);
+profileRouter.get('/', Authorization, getProfile);
+profileRouter.put('/', Authorization, updateProfile); 

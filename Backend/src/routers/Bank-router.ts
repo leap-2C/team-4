ab07ae7ef6756aa8fc242a43fdae1956d1d17426
hfshsiflkd <toogiit0521@gmail.com>
2025-04-11 12:30:31 +0800

    jijig zasvar

--- Backend/src/routers/Bank-router.ts
+++ Backend/src/routers/Bank-router.ts
@@ -2,11 +2,12 @@ import express from "express";
 import getCards from "../controller/Bank-Card/GetCards";
 import createCard from "../controller/Bank-Card/CreateCard";
 import updateCard from "../controller/Bank-Card/UpdateCard";
+import { Authorization } from "../MiddleWare/Authorization";
 
 const Bankrouter = express.Router();
 
-Bankrouter.get("/:userId", getCards);
-Bankrouter.post("/:userId", createCard);
-Bankrouter.patch("/:bankCardId", updateCard);
+Bankrouter.get("/:userId",Authorization, getCards);
+Bankrouter.post("/:userId",Authorization, createCard);
+Bankrouter.patch("/:bankCardId",Authorization ,updateCard);
 
 export default Bankrouter;

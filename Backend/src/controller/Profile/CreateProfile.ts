0e45dbb4891955fc6239ba7d40e829c64c8848d8
hfshsiflkd <toogiit0521@gmail.com>
2025-04-11 10:31:04 +0800

    aldaa zasav

--- Backend/src/controller/Profile/CreateProfile.ts
+++ Backend/src/controller/Profile/CreateProfile.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "../../utils/PrismaClient";
 
 import { Request, Response } from "express";
 
